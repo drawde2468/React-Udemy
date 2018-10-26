3096c08d887618ccdd732f8709d47515a6638951
Edward Leslie <edwardleslie09@gmail.com>
2018-10-25 21:01:53 -0400

    added context reducer for actions

--- contactmanager/src/components/Contacts.js
+++ contactmanager/src/components/Contacts.js
@@ -1,42 +1,22 @@
 import React, { Component } from "react";
 import Contact from "./Contact";
+import { Consumer } from "../context";
 
 class Contacts extends Component {
-  constructor() {
-    super();
-    this.state = {
-      contacts: [
-        {
-          id: 1,
-          name: "John Doe",
-          email: "jdoe@gmail.com",
-          phone: "555-555-5555"
-        },
-        {
-          id: 2,
-          name: "Karen Williams",
-          email: "kw@gmail.com",
-          phone: "444-444-4444"
-        },
-        {
-          id: 3,
-          name: "Henry Johnson",
-          email: "hj@gmail.com",
-          phone: "333-333-3333"
-        }
-      ]
-    };
-  }
-
   render() {
-    const { contacts } = this.state;
-
     return (
-      <React.Fragment>
-        {contacts.map(contact => (
-          <Contact key={contact.id} contact={contact} />
-        ))}
-      </React.Fragment>
+      <Consumer>
+        {value => {
+          const { contacts } = value;
+          return (
+            <React.Fragment>
+              {contacts.map(contact => (
+                <Contact key={contact.id} contact={contact} />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
